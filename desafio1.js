10483e5619a1ee8663cab0ddd45c26387dac8916
KarimRod95 <karimrodriguezg95@gmail.com>
2023-10-09 19:50:20 -0300

    First Commit

--- desafio1.js
+++ desafio1.js
@@ -1,24 +1,37 @@
-class ProductManager {
+const fs = require('fs');
 
-    constructor(){
-        this.products = []
+const path = './productsDB.json';
+
+class ProductManager {
+    constructor() {
+        this.products = [];
+        this.path = path;
+        this.loadProducts();
+    }
+
+    loadProducts() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf-8');
+            this.products = JSON.parse(data);
+        } catch (error) {
+            console.error('Error al cargar el archivo', error);
+        }
     }
 
     productID = () => {
-        const count = this.products.length
-        if(count === 0) return 1
+        const count = this.products.length;
+        if (count === 0) return 1;
 
-        const lastProduct = this.products[count-1]
+        const lastProduct = this.products[count - 1];
 
-        return lastProduct.id + 1
+        return lastProduct.id + 1;
     }
 
-    addProduct = ({title, description, price, thumbnail, code, stock}) => {
+    addProduct = ({ title, description, price, thumbnail, code, stock }) => {
+        if (!title || !description || !price || !thumbnail || !code || !stock) return;
+        if (this.products.some(product => product.code === code)) return;
 
-        if (!title || !description || !price || !thumbnail || !code || !stock) return // Valida que los campos sean obligatorios
-        if (this.products.some(product => product.code === code)) return //Valida que el campo code no se repita
-
-        const id = this.productID()
+        const id = this.productID();
 
         this.products.push({
             id,
@@ -28,18 +41,49 @@ class ProductManager {
             price,
             thumbnail,
             stock
-        })
+        });
+
+        this.saveProducts();
+    }
+
+    updateProduct = (id, updatedFields) => {
+        const productIndex = this.products.findIndex(product => product.id === id);
+
+        if (productIndex !== -1) {
+            const updatedProduct = { ...this.products[productIndex], ...updatedFields };
+            this.products[productIndex] = updatedProduct;
+            this.saveProducts();
+        } else {
+            console.error('Producto no encontrado');
+        }
+    }
+
+    deleteProduct = (id) => {
+        const productIndex = this.products.findIndex(product => product.id === id);
+
+        if (productIndex !== -1) {
+            this.products.splice(productIndex, 1); // Elimina el producto del arreglo
+            this.saveProducts();
+        } else {
+            console.error('Producto no encontrado');
+        }
+    }
+
+    saveProducts() {
+        const productsJSON = JSON.stringify(this.products);
+        fs.writeFileSync(this.path, productsJSON, 'utf-8');
+        console.log('Productos guardados en ' + this.path);
     }
 
     getProductByID = (code) => {
-        const product = this.products.find(p => p.code === code)
+        const product = this.products.find(p => p.code === code);
 
-        if(product) return product
-        else console.error('Not Found')
+        if (product) return product;
+        else console.error('No encontrado');
     }
 }
 
-const productManager = new ProductManager()
+const productManager = new ProductManager();
 
 productManager.addProduct({
     title: 'Beers',
@@ -48,24 +92,8 @@ productManager.addProduct({
     thumbnail: 'https://tinyurl.com/brahmabeer',
     code: '1001',
     stock: 100
-})
-productManager.addProduct({
-    title: 'Soda',
-    description: 'Coca-Cola',
-    price: 3,
-    thumbnail: 'https://tinyurl.com/coquitachica',
-    code: '1002',
-    stock: 100
-})
+});
 
-productManager.addProduct({
-    title: 'Wine',
-    description: 'Noths',
-    price: 8,
-    thumbnail: 'https://tinyurl.com/nothswine',
-    code: '1003',
-    stock: 100
-})
 productManager.addProduct({
     title: 'Chips',
     description: 'Lays',
@@ -75,6 +103,24 @@ productManager.addProduct({
     stock: 100
 })
 
-productManager.getProductByID('1001')
+
+productManager.updateProduct(2, {
+    title: 'Updated Chips',
+    description: 'Updated Lays',
+    price: 3,
+    thumbnail: 'https://tinyurl.com/layspack',
+    stock: 200
+})
+
+productManager.updateProduct(1, {
+    title: 'Updated Beers',
+    description: 'Updated Brahma',
+    price: 6,
+    thumbnail: 'https://tinyurl.com/brahmabeer',
+    stock: 200
+});
+
+// Eliminar un producto por su ID
+productManager.deleteProduct(1);
 
 console.log(productManager);
